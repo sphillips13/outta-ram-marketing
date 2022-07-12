48554d4dbe31b81841dc1b208f05ea5a85fa4980
sphillips13 <phi13025@byui.edu>
2022-07-11 18:32:03 -0600

    add ikaria

---
+++ IkariaLeanBellyJuice.php
@@ -0,0 +1,615 @@
+<!-- 
+Developed by Outta Ram Productions 7/11/2022
+
+Affiliate Link for clickbank:
+https://c67a882sy-t3yle9e72dd1ep4f.hop.clickbank.net
+-->
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ikaria Lean Belly Juicest</title>
+  <meta name="description" content="The Ikaria Lean Belly Juice is a delicious all-natural, scientifically-backed juice formula that burns fat quickly and effectively.">  <meta name="keywords" content="Lean belly juice">    <meta property="og:url" name="url" content="https://leanbellyjuice.com/">      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+  <meta charset="utf-8">
+  <link rel="icon" type="image/png" href="/images/favicon.png?v2" />
+
+  <!-- Gooogle Code Snipit for Add -->
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5814556169214942"
+     crossorigin="anonymous"></script>
+
+</head>
+<body>
+  
+<?php
+?>
+
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100;200;300;400;500;600;700&amp;display=swap" rel="stylesheet" />
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&amp;display=swap" rel="stylesheet" />
+<link href="/css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="/css/main.css?v2.21" rel="stylesheet" type="text/css" />
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css" /> <link rel="stylesheet" href="/discover-2/css/styles.css?v2" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" /> <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&amp;display=swap" rel="stylesheet" /> <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;display=swap" rel="stylesheet" /> <link href="/discover/css/extra.css?v3.1" rel="stylesheet" type="text/css" />
+<style>
+ 
+  .get-btn {
+    text-align: center;
+    margin: 40px 0;
+}
+  
+  .get-btn a
+  { background: #0781e3;
+    color: #fff;
+    padding: 20px 45px;
+    border-radius: 3px;
+    text-decoration: none !important;
+  }
+  
+  
+  body
+  { 
+      font-family: 'Poppins',sans-serif !Important;
+  }
+  
+  p {
+    font-size: 19px;
+    line-height: 150%;
+  }
+  
+  #cfs {
+    background-image: url(/images/click-to-play-sound.png?v2);
+    cursor:pointer;
+  }
+  
+  ul.beet {
+    margin-left: 0 !important;
+}
+  
+.beet li {
+    padding: 20px;
+}  
+  
+  .cus-title
+  { text-align:center;
+  }
+  
+  .cus {
+    padding: 60px 0;
+}
+  
+  
+  .top-sci
+  { font-size: 30px !important;
+    line-height: 139%;
+    text-align:center;
+  }
+
+  #accordion .panel-title
+  { margin-bottom:0 !important;
+  }
+  
+  #accordion .panel-title a
+  { display:block;
+    text-decoration:none !important;
+  }
+  
+ #top-b .navbar .container {
+    display: initial !important;
+  }
+  
+.video-wrapper
+  { margin:42px 16%;
+    border:5px solid #fff;
+  }
+  .purple-background
+  {
+  background: #0680e9;
+    background: linear-gradient(180deg, rgba(6,128,233,1) 0%, rgba(21,142,179,1) 58%, rgba(6,128,233,1) 100%);
+  }
+  .leaves
+  { background:none;
+  }
+  .blue-background
+  {
+   background: #0680e9;
+    background: linear-gradient(180deg, rgba(6,128,233,1) 0%, rgba(21,142,179,1) 58%, rgba(6,128,233,1) 100%);
+  }
+  
+  .pad-btm
+  { padding-bottom:20px !important;
+  }
+  
+  .index-column-2 {
+    background-image: url(/discover-2/images/b1.jpg?v2);
+}
+  .box
+  { background: linear-gradient(to bottom, #ffffff 0%, #e6e7e8 100%);
+    border: 1px solid rgb(8 130 226);
+    border-radius:0;
+    padding: 30px 35px 35px !important;
+  }
+  .pricea, .priceb, .pricec
+  { font-family: Poppins, sans-serif;
+    font-size: 40px !important;
+    letter-spacing: -0.5px;
+    font-weight: 700 !important;
+    color: rgb(40, 40, 40);
+    margin: 0;
+  }
+  
+  .days {
+    font-family: Poppins, sans-serif;
+    font-size: 30px !important;
+    line-height: 1.3em;
+    font-weight: 500 !important;
+    margin-top: 5px !important;
+    margin-bottom: 15px !important;
+}
+  
+ .ingredientimg {
+    margin-bottom: 12px;
+}
+  
+.gt-bt {
+    margin-bottom: 15px !important;
+} 
+
+.gt-bt-1 {
+    line-height: 120% !important;
+  margin-left: 40px !important;
+}
+  
+ .bonus-box-nw {
+    background: #fff;
+    margin: 30px 0;
+    padding: 25px;
+    box-shadow: 0px 0px 10px rgb(0 0 0 / 50%);
+    border-radius: 5px;
+}
+
+.bonus-box-nw h4 {
+    font-size: 28px;
+    font-weight: bold;
+    color: #f00;
+}
+
+.bonus-box-nw h5 {
+    font-weight: bold;
+    font-size: 22px;
+}
+
+.bonus-box-nw p {
+    font-size: 20px;
+    color: #000;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+    margin-bottom: 15px;
+}
+.free {
+    color: red;
+}
+
+.rw-flx{
+	display:flex;
+}
+
+
+  
+.pro-flx{
+	display:flex;
+}
+
+.product-sec {
+    margin-top: 5%;
+    margin-bottom: 5%;
+}
+.outr-wrp-pro {
+    border-radius: 16px;
+    box-shadow: 1px 4px 16px rgb(0 0 0 / 20%);
+    -webkit-transition: all 0.5s ease;
+}
+
+.price-box {
+    transition: transform 0.2s;
+}
+
+.bottle-item {
+    padding: 30px 35px 35px;
+    background: linear-gradient(to bottom, #ffffff 0%, #e6e7e8 100%);
+    border: 1px solid rgb(8 130 226);
+    /* border-radius: 11px; */
+    text-align: center;
+	 min-height: 760px;
+}
+
+.bottle-item h2 {
+    font-family: Poppins, sans-serif;
+    font-size: 40px;
+    letter-spacing: -0.5px;
+    font-weight: 700;
+    color: rgb(40, 40, 40);
+    margin: 0;
+}
+
+.bottle-item h3 {
+    font-family: Poppins, sans-serif;
+    font-size: 30px;
+    line-height: 1.3em;
+    font-weight: 500;
+    color: #054120;
+    margin-top: 5px !important;
+    margin-bottom: 15px;
+}
+
+.bottle-item .price-per-bottle {
+    font-family: "Oswald", sans-serif;
+    padding-top: 18px;
+    color: rgb(6, 64, 114);
+    line-height: 45px;
+}
+
+.bottle-item .price-large {
+    font-family: "Oswald", sans-serif;
+    font-size: 84px;
+    font-weight: bold;
+    padding-right: 10px;
+}
+
+.bottle-item .per-bottle-span {
+    font-family: "Oswald", sans-serif;
+    font-size: 30px;
+    line-height: 30px;
+    text-align: left;
+    display: inline-block;
+}
+
+.bottle-item .money-saving {
+    font-family: Poppins, sans-serif;
+    font-size: 18px;
+    font-weight: 700;
+    color: rgb(204, 0, 0);
+    min-height: 0px;
+}
+
+
+
+.pack-cards {
+    margin-top: 8px;
+}
+
+.bottle-item .price-data {
+    padding-top: 25px;
+    max-width: 460px;
+    margin: 0 auto;
+    font-family: "IBM Plex Sans", sans-serif;
+    font-size: 18px;
+    font-weight: 400;
+    color: rgba(33, 37, 41, 0.8);
+}
+
+.pro-Guarantee {
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    font-size: 19px;
+    margin-top: 4px;
+}
+
+
+
+.pro-btn.by-btn {
+    margin-top: 35%;
+}
+
+.free-offer {
+  margin-top: 10px;
+  margin-bottom: 15px;
+}
+
+.price-box:hover {
+    transform: scale(1.05);
+}
+
+#middle-item {
+    padding-top: 30px;
+	border: 2px solid #0882e2;
+}
+
+.pad-15{
+	padding-left:15px;
+	padding-right:15px;
+}
+
+.container-limit-2{
+	max-width: 1080px !important;
+}
+
+.hidden-lg {
+    display: none !important;
+}
+
+.hidden-xs{
+	 display: block !important;
+}  
+  
+.imgri{
+  border-radius: 100%;
+}
+  
+.glu-logo {
+    width: 168px;
+    margin-top: 10px;
+}
+  .sci-ref
+  { font-size:11px;
+    color: grey;
+    font-weight:300;
+  }
+  
+  
+  .anti-aging {
+    max-width:100%;
+} 
+.top-bar .navbar-default {
+    background: #000;
+  }
+  
+  .navbar-fixed-top {
+    top: 0;
+    position: fixed !important;
+}
+
+
+  
+  
+  @media only screen and (max-width:480px)
+  {
+    
+    .first-sec
+    { padding-bottom:0 !important;
+    }
+    
+    #top-b .navbar .container {
+     display: block !important; 
+}
+    
+  .video-wrapper
+  { margin:48px 0 0;
+    border:none;
+  }    
+ 
+ .rw-flx{
+	display:block;
+}
+
+.anti-aging {
+    margin: 0 auto;
+} 
+
+.vip {
+    margin: 0 auto;
+	padding-bottom: 10px;
+}    
+
+ .pro-flx{
+	display:block;
+}   
+      
+
+.hidden-lg {
+    display: block !important;
+}
+
+.hidden-xs{
+	 display: none !important;
+}
+
+.pro-btn.by-btn {
+    margin-top: 10px;
+}
+
+.price-box{
+	margin-bottom: 20px;
+  min-height:inherit;
+}
+
+.bottle-item h2 {
+    font-size: 30px;
+}
+
+.bottle-item h3 {
+    font-size: 20px;
+}
+
+.bottle-item .price-large {
+    font-size: 60px;
+}
+
+.pack-cards {
+    width: 80%;
+}
+
+.pro-btn{
+	width:86%;
+}   
+    
+.gt-bt-1 {
+    line-height: 140% !important;
+ 
+}
+    
+#accordion .panel-title a {
+    font-size: 22px;
+    line-height: 140%;
+}    
+    
+  }
+  
+</style>
+
+<!-- Video Header 
+<section>
+        <div align="center">
+            <iframe src="https://player.vimeo.com/video/701212890?h=e1e2283afc&amp; width="40%" height="40%" frameborder="0" autoplay; fullscreen; picture-in-picture"  title="LBJ Reorder.mp4" data-ready="true">
+            </iframe>
+        </div>
+</section>
+-->
+
+<!-- Blue Banner Product Picture -->
+<section class="section blue-background is-paddingless">
+    <div class="container container-limit has-text-white px-2 pt-5 pb-4">
+        <div class="columns">
+            <div class="column has-text-centered"><img src="https://leanbellyjuice.com/discover-2/images/full-product.png" style="width: 400px; height: 250px;" href="https://c67a882sy-t3yle9e72dd1ep4f.hop.clickbank.net" /></div>
+            <div class="column">
+                <p>The Ikaria Lean Belly Juice is a potent blend of exotic nutrients specifically developed to rid your body of the newly discovered root cause of stubborn fat accumulation&hellip; destructive ceramide compounds.</p>
+                <p>The Ikaria Lean Belly Juice flushes out ceramides and renews your body from within&hellip;</p>
+                <p>Allowing you to dissolve stubborn, clogged fat easily and flood your body with newfound energy and vitality,</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- Get Started Button -->
+<section>  
+  <div class="get-btn" id="gtbtn" >
+      <a href="https://c67a882sy-t3yle9e72dd1ep4f.hop.clickbank.net">Watch a Video Here</a> 
+  </div>
+</section>
+
+<!-- Scientists Discovered -->
+<section class="section is-paddingless">
+  <div class="container container-limit px-2 py-5">
+      <div class="columns mb-5 is-mobile">
+          <div style="text-align: center">
+              <p class="title is-size-1-desktop is-size-2-tablet is-size-3-mobile is-capitalized lh1 mb-2 top-sci">Top Scientists Finally Reveal The True Reason Why You&rsquo;re Struggling To Lose Weight</p>
+              <p>And it has <b>far less to do</b> with what you eat and how much you exercise!</p>
+              <p>A recent discovery by the University of Alberta in Canada found that all overweight people have high levels of toxic lipid molecules&hellip; called <b>ceramides<sup>1</sup></b>.</p>
+              <p>These foreign compounds force fat cells to spill into your bloodstream after you eat. The toxic fat can accumulate around your vital organs: your liver, pancreas and heart and even your arteries<sup>17</sup>.</p>
+              <p>When these vital organs are clogged with fat, your metabolism slows to a crawl and your fat-burning hormones almost completely shut down.</p>
+              <p>Your body switches to starvation mode, which is when you store even more fat on your belly, back, buns, thighs and hips&hellip;</p>
+              <p>The good news is, there is a way to target ceramides and dissolve dangerous fat from around your organs fast&hellip; turning your body in a calorie-burning, fat melting machine.</p>
+              <p>It&rsquo;s the very same powerful juice that some of the leanest, healthiest and longest-living men and women drink every morning...</p>
+              <p>Introducing...The Ikaria Lean Belly Juice!</p>
+          </div>
+      </div>
+  </div>
+</section>
+
+<!-- Stats -->
+<section class="section is-paddingless blue-background">
+  <div class="container container-limit  px-2 py-5">
+    <div class="has-text-centered mb-3"><img src="https://leanbellyjuice.com/images/ikaria-white-logo.png" width="150" /></div>
+    <div class="columns is-mobile is-multiline is-variable is-1">
+      <div class="column is-one-third-desktop is-half-touch">
+        <div class="columns is-vcentered is-mobile is-variable is-2">
+          <div class="column is-3"><img src="https://leanbellyjuice.com/discover-2/images/thumb.png?v1" /></div>
+          <div class="column">
+            <p class="block-13">100% Natural Formula</p>
+          </div>
+        </div>
+      </div>
+    <div class="column is-one-third-desktop is-half-touch">
+      <div class="columns is-vcentered is-mobile is-variable is-2">
+        <div class="column is-3"><img src="https://leanbellyjuice.com/discover-2/images/thumb.png?v1" /></div>
+        <div class="column">
+          <p class="block-13">Easy To Mix</p>
+        </div>
+      </div>
+    </div>
+    <div class="column is-one-third-desktop is-half-touch">
+      <div class="columns is-vcentered is-mobile is-variable is-2">
+        <div class="column is-3"><img src="https://leanbellyjuice.com/discover-2/images/thumb.png?v1" /></div>
+        <div class="column">
+          <p class="block-13">Vegetarian</p>
+        </div>
+      </div>
+    </div>
+    <div class="column is-one-third-desktop is-half-touch">
+      <div class="columns is-vcentered is-mobile is-variable is-2">
+        <div class="column is-3"><img src="https://leanbellyjuice.com/discover-2/images/thumb.png?v1" /></div>
+        <div class="column">
+          <p class="block-13">No Stimulants</p>
+        </div>
+      </div>
+    </div>
+    <div class="column is-one-third-desktop is-half-touch">
+      <div class="columns is-vcentered is-mobile is-variable is-2">
+        <div class="column is-3"><img src="https://leanbellyjuice.com/discover-2/images/thumb.png?v1" /></div>
+      <div class="column">
+        <p class="block-13">Non-GMO</p>
+      </div>
+    </div>
+  </div>
+  <div class="column is-one-third-desktop is-half-touch">
+    <div class="columns is-vcentered is-mobile is-variable is-2">
+      <div class="column is-3"><img src="https://leanbellyjuice.com/discover-2/images/thumb.png?v1" /></div>
+      <div class="column">
+        <p class="block-13">Non-Habit Forming</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</section>
+
+<!-- Customer Reviews -->
+<section>
+  <div>
+    <p class="title is-size-1-desktop is-size-2-tablet is-size-3-mobile is-capitalized lh1 mb-2 top-sci">Satisfied Customer Reviews</p>
+  </div>
+
+  <div class="columns is-mobile is-variable is-0 mb-5">
+    <div class="column is-1 pr-2"><img src="https://leanbellyjuice.com/discover-2/images/test-1.jpg" class="t-image" width="100%" /></div>
+    <div class="column">
+      <p class="has-text-weight-bold mb-2 is-size-3 lh1 gt">Greg has dropped a staggering 27.5lbs</p>
+      <p class="verified mb-2 is-size-5"><img src="https://leanbellyjuice.com/discover-2/images/rating.png" alt="" width="90" /> Verified Purchase</p>
+      <p class="mb-2"><img src="https://leanbellyjuice.com/discover-2/images/quote.jpg" style="padding-right: 10px;" width="39" /> All that I can say is that I love this. I&rsquo;m down a pant size in less than 3 weeks. I can&rsquo;t even believe it. It&rsquo;s so unexpected because I still enjoy my regular food, plus wine and beer a few times a week. The best thing is I know that I&rsquo;m losing weight the healthy way.</p>
+      <p class="is-size-5"><strong>Greg R. </strong> &ndash; Akron, OH</p>
+    </div>
+  </div>
+  <div class="columns is-mobile is-variable is-0 mb-5">
+    <div class="column is-1 pr-2"><img src="https://leanbellyjuice.com/discover-2/images/test-2.jpg" class="t-image" width="100%" /></div>
+    <div class="column">
+      <p class="has-text-weight-bold mb-2 is-size-3 lh1 gt">Nancy is 32lbs lighter and still going strong</p>
+      <p class="verified mb-2 is-size-5"><img src="https://leanbellyjuice.com/discover-2/images/rating.png" alt="" width="90" /> Verified Purchase</p>
+      <p class="mb-2"><img src="https://leanbellyjuice.com/discover-2/images/quote.jpg" style="padding-right: 10px;" width="39" />Over the last 3 months, I&rsquo;m down an amazing 32 pounds! My mindset, self-image and outlook on life is better than ever. I&rsquo;m able to be more active with my children. My life has changed so dramatically and my children have a healthier mommy that can enjoy life with them.</p>
+      <p class="is-size-5"><strong>Nancy B. </strong> &ndash; Ann Arbor, MI</p>
+    </div>
+  </div>
+  <div class="columns is-mobile is-variable is-0">
+    <div class="column is-1 pr-2"><img src="https://leanbellyjuice.com/discover-2/images/test-3.jpg" class="t-image" width="100%" /></div>
+    <div class="column">
+      <p class="has-text-weight-bold mb-2 is-size-3 lh1 gt">Debbi obliterated 24lbs</p>
+      <p class="verified mb-2 is-size-5"><img src="https://leanbellyjuice.com/discover-2/images/rating.png" alt="" width="90" /> Verified Purchase</p>
+      <p class="mb-2"><img src="https://leanbellyjuice.com/discover-2/images/quote.jpg" style="padding-right: 10px;" width="39" />This morning I actually cried. Because when I stepped on the scale I was down almost 24lbs after only 8 weeks which is crazy. Every time I walk past a mirror I actually have to do a double-take because I&rsquo;m just looking at myself in a whole new way and people are as well. Sometimes I can&rsquo;t actually believe that this is me.</p>
+      <p class="is-size-5"><strong>Debbi S.</strong> - Henderson, NV</p>
+    </div>
+  </div>
+  <div class="columns is-centered is-mobile">
+    <div class="column is-9">
+      <div class="columns is-mobile is-multiline is-gapless product-shots">
+      <div class="column is-one-quarter-tablet is-half-mobile product-shot"></div>
+      <div class="column is-one-quarter-tablet is-half-mobile product-shot"></div>
+      <div class="column is-one-quarter-tablet is-half-mobile product-shot"></div>
+      <div class="column is-one-quarter-tablet is-half-mobile product-shot"></div>
+    </div>
+  </div>
+</div>
+</div>
+</section>
+
+
+<!-- Discount -->
+<section>
+  <div class="section is-paddingless blue-background">
+    <p style="color: white; text-align: center; font-size: 40px;">Experience the Life-Changing Effects of The Ikaria Lean Belly Juice at an Amazing Discount <a href="https://c67a882sy-t3yle9e72dd1ep4f.hop.clickbank.net" style="color: white;"><u>Here</u></a></p>
+  </div>
+</section>
